8abd88d5ccd0cf0cb2de25e795446a8fad601c68
amottier <amottier@online.net>
2021-01-25 13:30:30 +0100

    reponse statuscode based

--- handler/utils.go
+++ handler/utils.go
@@ -2,14 +2,19 @@ package handler
 
 import (
 	"encoding/json"
+	"fmt"
 	"io"
 	"io/ioutil"
 	"net/http"
 )
 
+var (
+	httpStatusOK = http.StatusOK
+)
+
 // ResponseHTTP - Type for HTTP triggers response emitted by function handlers
 type ResponseHTTP struct {
-	StatusCode      int               `json:"statusCode"`
+	StatusCode      *int              `json:"statusCode"`
 	Body            json.RawMessage   `json:"body"`
 	Headers         map[string]string `json:"headers"`
 	IsBase64Encoded bool              `json:"isBase64Encoded"`
@@ -25,15 +30,14 @@ func GetResponse(response io.Reader) (*ResponseHTTP, error) {
 		return nil, ErrorInvalidHTTPResponseFormat
 	}
 
-	err = json.Unmarshal(bodyBytes, &handlerResponse)
-	if err != nil {
-		// Body is not a valid JSON object
-		handlerResponse.Body = bodyBytes
-	}
+	unmarshalErr := json.Unmarshal(bodyBytes, &handlerResponse)
 
-	// If handler dit not return status code, just use 200 OK
-	if handlerResponse.StatusCode == 0 {
-		handlerResponse.StatusCode = http.StatusOK
+	// If handler dit not return a JSON or status code, just use 200 OK
+	if unmarshalErr != nil || handlerResponse.StatusCode == nil {
+		handlerResponse.StatusCode = &httpStatusOK
+		handlerResponse.Body = bodyBytes
+	} else {
+		fmt.Println("ok")
 	}
 
 	return handlerResponse, nil
